fea9bab4e1e73e4760f60a4d74c17ec0bfec9e89
Ansh Soni <anshs.dev@gmail.com>
2025-08-23 18:18:30 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-echo "Installing system dependencies..."
-apt-get update && apt-get install -y tesseract-ocr
-
-echo "Installing Python dependencies..."
-pip install -r requirements.txt
